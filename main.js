cf1edec3a87717be1c661d8c285d1da29bea5887
Lisa-2011 <93234101+Lisa-2011@users.noreply.github.com>
2023-01-29 20:38:56 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ function setup(){
     canvas=createCanvas(280,280);
     canvas.center();
     background('white');
+    canvas.mouseReleased(classifyCanvas);
 }
 
 function draw(){
@@ -52,4 +53,31 @@ function check_sketch(){
         answer_holder="";
         updateCanvas();
     }
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY)
+    }
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("drawn_sketch").innerHTML="Your Sketch: "+ results[0].label;
+    document.getElementById("confidence").innerHTML="Confidence: "+ Math.round(results[0].confidence * 100) + "%";
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
